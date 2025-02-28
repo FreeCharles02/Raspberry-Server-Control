3d5abc00d26ec81c7620235aac691b5d07ab24ee
Charles <105826694+FreeCharles02@users.noreply.github.com>
2025-02-28 15:33:51 -0600

    Bug fixing Serverside
    Updates iP and adds buffer

--- Raspberry.py
+++ Raspberry.py
@@ -4,25 +4,34 @@ import os
 import socket 
 
 
-host = '192.168.1.253' # All available interfaces
+host = '172.20.10.4' # All available interfaces
 port = 9999 # Non privledged port
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #TCP internet connection
-server.bind((host, port)) # Establishes connection between IP and port
+server.bind(("", port)) # Establishes connection between IP and port
 
 server.listen(1)
 ser = serial.Serial('/dev/ttyACM0', 9600)
+ser.write(struct.pack('!bbbb', 0, 0, 0, 0))
+
+print("gettin connection...")
+client, addr = server.accept()
+print("got a connection")
+
 
 #Checks constantly for client data
 while True: 
-    client, addr = server.accept()
-    client.send("Hello from RaspberryPi".encode())
-    lf = client.recv(1)
-    lb = client.recv(1)
-    rf = client.recv(1)
-    rb = client.recv(1)
+#    client.send("Hello from RaspberryPi".encode())
 
-    
+    buf = '';
+    while len(buf) < 4:
+        buf = client.recv(4)
+    lf, lb, rf, rb = struct.unpack('!bbbb', buf[:4])
+
+    print(f"\tA: {lf}\n" +
+          f"\tB: {lb}\n" +
+          f"\tC: {rf}\n" +
+          f"\tD: {rb}\n")
+
 
     ser.write(struct.pack('!bbbb', lf, lb, rf, rb))
-
