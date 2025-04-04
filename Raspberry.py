4006e9707eceb3a1f2124b5b409d0c678d43c03a
Legacy549 <90426955+Legacy549@users.noreply.github.com>
2025-04-04 14:04:28 -0500

    Update Raspberry.py

--- Raspberry.py
+++ Raspberry.py
@@ -11,7 +11,7 @@ from adafruit_motor import servo
 import busio
 # Set channels to the number of servo channels on your kit.
 # 8 for FeatherWing, 16 for Shield/HAT/Bonnet.
-kit = ServoKit(channels=8)
+kit = ServoKit(channels=16)
 
 i2c=busio.I2C(board.SCL, board.SDA)
 while not i2c.try_lock():
@@ -47,7 +47,7 @@ server.bind(("", port))
 server.listen(1)
 
 ser = serial.Serial('/dev/ttyACM0', 9600)
-ser.write(struct.pack('!BBBB', 0, 0, 0, 0))
+ser.write(struct.pack('!BBBBBBBB', 0, 0, 0, 0, 0, 0, 0, 0))
 
 
 def main():
@@ -57,40 +57,46 @@ def main():
 
     # Checks constantly for client data
     while True:
-        # client.send("Hello from RaspberryPi".encode())
-
+        client.send("Hello from RaspberryPi".encode())
+        pca1.frequency = 50
         buf = ''
         start = time.time()
         while (len(buf) < 12):
             buf = client.recv(12)
             if (time.time()-start) > 1:
                 return socket.timeout
-
-        rb, rf, lb, lf, lb_button, rb_button, dpad_value_1, dpad_value_2, lt_trigger, rt_trigger, a, y = struct.unpack('!BBBBBBBBBB', buf)
-        if(dpad_value_1 == 1):
+        
+        rb, rf, lb, lf, lb_button, rb_button, dpad_value_1, dpad_value_2, rt_trigger, lt_trigger,  x, b= struct.unpack('!BBBBBBBBBBBB', buf)
+        print("LB: ", lb_button, "RB: ", rb_button)
+        print("RT: ", rt_trigger, "LT: ", lt_trigger)
+        if(dpad_value_1 == 2):
+            print("Dpad: ", dpad_value_1)
             kit.continuous_servo[1].throttle = 1
             kit.continuous_servo[0].throttle = -1
-        elif(dpad_value_1 == -1):
+        elif(dpad_value_1 == 0):
+            print("Dpad: ", dpad_value_1)
             kit.continuous_servo[1].throttle = -1
             kit.continuous_servo[0].throttle = 1
         else:
+            print("Dpad: ", dpad_value_1)
             kit.continuous_servo[1].throttle = 0
             kit.continuous_servo[0].throttle = 0
 
-        if(dpad_value_2 == 1):     
-            kit.servo[2].angle = 180
-        elif(dpad_value_2 == -1):
+        if(x == 1):
+            print("B: ", b)
             kit.servo[2].angle = 0
-        if(y == 1):
+        else:
+            kit.servo[2].angle =180
+        if(b == 1):
+            print("X: ", x)
             kit.servo[3].angle = 180
         else:
-            kit.servo[3].angle = 0
-        if(a == 1):
-            kit.servo[4].angle = 180
-        else:
+            kit.servo[3].angle =0
+        if(dpad_value_2 == 0):
+            kit.servo[4].angle = 180 
+        elif(dpad_value_2 == 2):
             kit.servo[4].angle = 0
-                
-        ser.write(struct.pack('!BBBB', lf, lb, rf, rb))
+        ser.write(struct.pack('!BBBBBBBB', lf, lb, rf, rb, rt_trigger, lt_trigger, lb_button, rb_button))
 
 
 if __name__ == "__main__":
